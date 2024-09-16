94c97dfc565adb8de55c80d4981ada12a3dd2e2d
Muhammad Khizer <you@example.com>
2024-09-16 12:54:31 +0500

    Done

--- main.cpp
+++ main.cpp
@@ -7,19 +7,38 @@ using namespace std;
 // `pos` specifies the index to sort by if sorting a vector of pairs.
 template <typename T>
 void stableSelectionSort(vector<T>& a) {
-    
+     int n = a.size();
+        for (int i = 0; i < n-1; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if (a[j] < a[min]) {
+                min = j;
+            }
+            }
+            T minval = a[min];
+            for (int k = min; k > i; --k) {
+            a[k] = a[k - 1];
+        }
+        a[i] = minval;
+        }
 }
 
 // Unstable Selection Sort given in slides
 template <typename T>
 void selectionSort(vector<T>& a) {
      int n = a.size();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             int min = i;
             for (int j = i+1; j < n; j++) {
-                if (a[j] < a[min]) min = j;
+                if (a[j] < a[min]) {
+                min = j;
             }
-            std::swap(a[i], a[min]);
+            }
+            T minval = a[min];
+            for (int k = min; k > i; --k) {
+            a[k] = a[k - 1];
+        }
+        a[i] = minval;
         }
 }
 int main() {
@@ -34,7 +53,7 @@ int main() {
     cout << endl;
 
     // Sort by the second element of the pairs
-    selectionSort(arr);
+    stableSelectionSort(arr);
 
     // Print sorted array
     cout << "Sorted array by the first element:" << endl;
